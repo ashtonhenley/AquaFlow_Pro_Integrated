7ece577e32921874af41a97d99c2d702230c5905
k-rubbo-1437 <khalinrubbo@gmail.com>
2026-04-14 13:17:59 -0400

    percent sign removed

--- Core/Src/LCD.c
+++ Core/Src/LCD.c
@@ -181,7 +181,7 @@ void LCD_ShowReservoirProgress(const char* progress) {
 	LCD_SetCursor(40);
 	LCD_WriteString("CURRENT   ");
 	LCD_WriteString(progress);
-	LCD_WriteCharacter('%');
+	LCD_WriteCharacter(' cm');
 }
 
 void LCD_ShowDrainScreen(uint8_t estimate_seconds) {
